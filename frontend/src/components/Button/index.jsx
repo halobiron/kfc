d476611bc97106c9cc1c1a70040185b3fa54a7c4
team3hailong <LongTH.B23CN510@stu.ptit.edu.vn>
2026-02-11 11:50:56 +0700

    feat: Add a new reusable Button component with styling and routing capabilities.

--- frontend/src/components/Button/index.jsx
+++ frontend/src/components/Button/index.jsx
@@ -5,70 +5,60 @@ import './Button.css';
 
 const Button = React.forwardRef(({
     children,
-    variant = 'primary', // primary, secondary
-    size = 'md',         // sm, md, lg
+    variant = 'primary',
+    size = 'md',
     loading = false,
     disabled = false,
     fullWidth = false,
-    startIcon = null,
+    startIcon,
     className = '',
-    component = 'button', // can be 'button', 'a', Link, or motion.button
+    component,
     to,
     href,
     type = 'button',
     ...props
 }, ref) => {
+    let Component = component || 'button';
+    if (to) Component = Link;
+    else if (href) Component = 'a';
 
-    const Component = component;
+    const isLink = to || href || Component === 'a' || Component === Link;
 
-    // Logic to determine if we should use Link or a native anchor
-    const isLink = component === Link || (typeof component === 'string' && component.toLowerCase() === 'a') || to || href;
-    const FinalComponent = to ? Link : (href ? 'a' : Component);
-
-    const baseClass = 'kfc-btn';
-    const classes = [
-        baseClass,
-        `${baseClass}--${variant}`,
-        `${baseClass}--${size}`,
-        fullWidth ? `${baseClass}--full` : '',
-        disabled || loading ? 'disabled' : '',
+    const buttonClass = [
+        'kfc-btn',
+        `kfc-btn--${variant}`,
+        `kfc-btn--${size}`,
+        fullWidth && 'kfc-btn--full',
+        (disabled || loading) && 'disabled',
         className
     ].filter(Boolean).join(' ');
 
-    const otherProps = {
-        ref,
-        className: classes,
-        disabled: disabled || loading,
-        ...props
-    };
-
-    if (to) otherProps.to = to;
-    if (href) otherProps.href = href;
-    if (!isLink) otherProps.type = type;
-
     return (
-        <React.Fragment>
-            {/* @ts-ignore */}
-            <FinalComponent {...otherProps}>
-                {loading && (
-                    <span className="kfc-btn-spinner"></span>
-                )}
-
-                {!loading && (
-                    <>
-                        {startIcon && <span className="kfc-btn-start-icon">{startIcon}</span>}
-                        {children}
-                    </>
-                )}
-            </FinalComponent>
-        </React.Fragment>
+        <Component
+            ref={ref}
+            className={buttonClass}
+            disabled={!isLink ? (disabled || loading) : undefined}
+            type={!isLink ? type : undefined}
+            to={to}
+            href={href}
+            {...props}
+        >
+            {loading ? (
+                <span className="kfc-btn-spinner" />
+            ) : (
+                <>
+                    {startIcon && <span className="kfc-btn-start-icon">{startIcon}</span>}
+                    {children}
+                </>
+            )}
+        </Component>
     );
 });
 
 Button.propTypes = {
     children: PropTypes.node,
-    variant: PropTypes.oneOf(['primary', 'secondary']),
-    size: PropTypes.oneOf(['sm', 'md', 'lg']),
+    variant: PropTypes.string,
+    size: PropTypes.string,
     loading: PropTypes.bool,
     disabled: PropTypes.bool,
     fullWidth: PropTypes.bool,
