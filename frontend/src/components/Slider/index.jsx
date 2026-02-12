52bfd9b13cc0ddf364facadda7f3c06a7350680f
team3hailong <LongTH.B23CN510@stu.ptit.edu.vn>
2026-02-12 11:10:02 +0700

    refactor: enhance Slider component by simplifying slide rendering and improving structure

--- frontend/src/components/Slider/index.jsx
+++ frontend/src/components/Slider/index.jsx
@@ -3,33 +3,33 @@ import { Link } from 'react-router-dom'
 import slide1 from '../../assets/images/banners/slide1.png'
 import slide2 from '../../assets/images/banners/slide2.png'
 
+const CAROUSEL_ID = 'carouselExampleControls'
+const slides = [slide1, slide2]
+
 const Slider = () => {
-    return (
-        <div className='slider-wrapper'>
-            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <Link to="/products">
-                            <img src={slide1} className="d-block w-100" alt="..." />
-                        </Link>
-                    </div>
-                    <div className="carousel-item">
-                        <Link to="/products">
-                            <img src={slide2} className="d-block w-100" alt="..." />
-                        </Link>
-                    </div>
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
+  return (
+    <div className="slider-wrapper">
+      <div id={CAROUSEL_ID} className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          {slides.map((slide, index) => (
+            <div key={slide} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+              <Link to="/products">
+                <img src={slide} className="d-block w-100" alt={`Banner ${index + 1}`} />
+              </Link>
             </div>
+          ))}
         </div>
-    )
+        <button className="carousel-control-prev" type="button" data-bs-target={`#${CAROUSEL_ID}`} data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target={`#${CAROUSEL_ID}`} data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Slider
